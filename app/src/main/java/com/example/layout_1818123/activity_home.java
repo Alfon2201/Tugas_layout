ac3669316b8353d72d19841a467d670af9fc2ef5
ALFONSUS VITO <alfvitoputra223@gmail.com>
2020-10-19 10:44:20 +0700

    first Commit

---
+++ app/src/main/java/com/example/layout_1818123/activity_home.java
@@ -0,0 +1,14 @@
+package com.example.layout_1818123;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
